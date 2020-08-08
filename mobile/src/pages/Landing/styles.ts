8883de8e8cca56cc342e4365b3ac9981bd0ac770
Gabriel <gabriel_g_alves@hotmail.com>
2020-08-08 23:00:17 +0100

    fonts

--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -13,14 +13,43 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   title: {
+    fontFamily: "Poppins_400Regular",
     color: "#FFF",
     fontSize: 20,
     lineHeight: 30,
     marginTop: 80,
   },
   titleBold: {
-      fontWeight: 'bold'
-  }
+    fontFamily: "Poppins_600SemiBold",
+  },
+
+  buttonsContainer: {
+    flexDirection: "row",
+    marginTop: 40,
+    justifyContent: "space-between",
+  },
+
+  button: {
+    height: 150,
+    width: "48%",
+    backgroundColor: "#333",
+    borderRadius: 8,
+    padding: 24,
+    justifyContent: "space-between",
+  },
+
+  buttonPrimary: {
+    backgroundColor: "#9871f5",
+  },
+  buttonSecondary: {
+    backgroundColor: "#04d361",
+  },
+
+  buttonText: {
+    fontFamily: "Archivo_700Bold",
+    color: "#FFF",
+    fontSize: 20,
+  },
 });
 
 export default styles;
